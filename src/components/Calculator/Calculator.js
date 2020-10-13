614b42e464fa542bdf41ff92ef7cbfa739a709bb
Joel Roske <joel.j.roske@gmail.com>
2020-10-13 14:46:47 -0500

    fixed clear and delete button bugs

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -43,10 +43,12 @@ class Calculator extends Component {
       });
   };
 
+  //   Handles the click of any button
   handleClick = (index) => {
     const { displayValue, operator, firstVal, secondVal, nextVal } = this.state;
 
     switch (index) {
+      // If a number is pressed, it is added to displayValue
       case "0":
       case "1":
       case "2":
@@ -57,6 +59,7 @@ class Calculator extends Component {
       case "7":
       case "8":
       case "9":
+        //This logic determines if numbers are set to first or second values
         this.setState({
           displayValue: displayValue === "0" ? index : displayValue + index,
         });
@@ -74,37 +77,41 @@ class Calculator extends Component {
       case "-":
       case "x":
       case "/":
-        this.setState({
-          operator: index,
-          nextVal: true,
-          displayValue:
-            (operator !== null
-              ? displayValue.substr(0, displayValue.length - 1)
-              : displayValue) + index,
-        });
+        //This sets the operator, as well as prevent two operators from being entered
+        if (!operator) {
+          this.setState({
+            operator: index,
+            nextVal: true,
+            displayValue:
+              (operator !== null
+                ? // This allows the operator to be changed
+                  displayValue.substr(0, displayValue.length - 1)
+                : displayValue) + index,
+          });
+        }
         break;
 
       case ".":
-        //return to prevent double decimals like 4.4.4
-        if(!displayValue.includes('.')){
-        let decimal = displayValue.slice(-1); //gets last character
-        this.setState({
-          displayValue: decimal !== "." ? displayValue + index : displayValue,
-        });
-        if (!nextVal) {
+        //This adds decimal to a value and prevents multiple decimals
+        if (!displayValue.includes(".")) {
+          let decimal = displayValue.slice(-1); //gets last character
           this.setState({
-            firstVal: firstVal + index,
+            displayValue: decimal !== "." ? displayValue + index : displayValue,
           });
-        } else {
-          this.setState({
-            secondVal: secondVal + index,
-          });
-        }}
+          if (!nextVal) {
+            this.setState({
+              firstVal: firstVal + index,
+            });
+          } else {
+            this.setState({
+              secondVal: secondVal + index,
+            });
+          }
+        }
         break;
       case "=":
-        //call POST to server
+        //sends values to server to be calculated, and resets state
         this.addCalculation(firstVal, operator, secondVal);
-        console.log(this.state);
         this.setState({
           displayValue: "0",
           operator: null,
@@ -113,7 +120,8 @@ class Calculator extends Component {
           nextVal: false,
         });
         break;
-      case "CLR":
+      case "CLEAR":
+        //This resets state
         this.setState({
           displayValue: "0",
           operator: null,
@@ -123,13 +131,20 @@ class Calculator extends Component {
         });
         break;
 
-      case "DEL":
-        let string = displayValue.toString();
-        let backspace = string.substr(0, string.length - 1);
-        let length = string.length;
+      case "DELETE":
+        //This removes the last char from display
         this.setState({
-          displayValue: length === 1 ? "0" : backspace,
+          displayValue: displayValue.slice(0, -1),
         });
+        if (!nextVal) {
+          this.setState({
+            firstVal: firstVal.slice(0, -1),
+          });
+        } else {
+          this.setState({
+            secondVal: secondVal.slice(0, -1),
+          });
+        }
         break;
       default:
         return this.state;
@@ -155,18 +170,25 @@ class Calculator extends Component {
       "=",
       "+",
     ];
+    const bigButtons = ["CLEAR", "DELETE"];
     return (
       <>
-        <div style={styles.outerCalculator}>
-          <h2>Calculator</h2>
+        <div>
+          <h1>Calculator</h1>
           <div style={styles.calculatorContainer}>
-            <h4 style={styles.calculatorTitle}>
-              JOEL <span style={styles.model}>SEZZLE-2020</span>
-            </h4>
             <div style={styles.calcInput}>{this.state.displayValue}</div>
             <div style={styles.numberContainer}>
-              <button style={styles.largeButton}>CLEAR</button>
-              <button style={styles.largeButton}>DELETE</button>
+              {/* I wanted some buttons styled differently, so
+                I mapped two different arrays of them */}
+              {bigButtons.map((button, index) => (
+                <button
+                  key={index}
+                  style={styles.largeButton}
+                  onClick={() => this.handleClick(button)}
+                >
+                  {button}
+                </button>
+              ))}
               <br />
               {buttons.map((button, index) => (
                 <button
@@ -180,11 +202,14 @@ class Calculator extends Component {
               <br />
             </div>
           </div>
-          <ul>
-            {this.state.calculation.map((calc) => (
-              <li key={calc.id}>{calc.calculation}</li>
-            ))}
-          </ul>
+          <div>
+            <h1>Past 10 Calculations</h1>
+            <ul>
+              {this.state.calculation.map((calc) => (
+                <li key={calc.id}>{calc.calculation}</li>
+              ))}
+            </ul>
+          </div>
         </div>
       </>
     );
@@ -192,32 +217,24 @@ class Calculator extends Component {
 }
 
 const styles = {
-  calculatorTitle: {
-    color: "white",
-    fontSize: "19px",
-    margin: "2px 0px 12px 0px",
-    textAlign: "center",
-    padding: "0px",
-  },
-  model: {
-    color: "#b07080",
-  },
   calcInput: {
     padding: "2px 36px 2px 10px",
     borderRadius: "5px",
+    border: "1px groove gray",
     width: "75%",
-    // font-family: "Changa", sans-serif,
+    fontFamily: `serif`,
     fontSize: "25px",
     textAlign: "right",
-    margin: "10px",
+    alignItems: "center",
+    margin: "13px",
     backgroundColor: "white",
   },
   numberContainer: {
     textAlign: "center",
   },
   calculatorContainer: {
-    border: "4px solid #000000",
-    backgroundColor: "#434b60",
+    border: "4px groove gray",
+    backgroundColor: "#2e465d",
     padding: "25px 0px 50px 0px",
     borderRadius: "5px",
     margin: "auto",
@@ -228,21 +245,23 @@ const styles = {
     margin: "6px 4px",
     padding: "15px 40px 15px 20px",
     width: "2.8%",
-    backgroundColor: "#9897a4",
-    border: "1.5px groove #2a2a38",
+    backgroundColor: "black",
+    border: "1px groove gray",
     borderRadius: "5px",
     color: "white",
     fontSize: "30px",
+    fontWeight: "bold",
   },
   largeButton: {
     margin: "3px 4px",
     padding: "5px 20px",
     width: "135px",
-    backgroundColor: "#b07080",
-    border: "1.5px groove #2a2a38",
+    backgroundColor: "#aa2365",
+    border: "1px groove gray",
     borderRadius: "5px",
     color: "white",
     fontSize: "20px",
+    fontWeight: "bold",
   },
 };
 export default Calculator;
